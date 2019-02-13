e8bb2e83624704983c91d8f4c21b831b1739dd92
mzgora <m.zgorzyk@gmail.com>
2019-02-13 23:51:47 +0100

    99 Bottles of beer

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,24 @@
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.print("Hello World!");
+	public static void main(String[] args) 
+	 {
+		System.out.println("Hello World!\n");
+		
+		int butelka_1 = 100;
+		int butelka_2 = 98;
+
+	String tekst1 = "bottles of beer on the wall,";
+	String tekst2 = "bottles of beer.";
+	String tekst3 = "Take one down and pass it around -"; 
+	String tekst4 = "no more bottles of beer on the wall."; 
+	    for(int i=0; i<98; i++)
+	    {
+	      System.out.println(--butelka_1 +" "+tekst1+" "+ butelka_1 +" "+tekst2);	  
+	      System.out.println(tekst3+" "+ butelka_2-- +" "+tekst2);
+	      System.out.println();
+	    }
+	    
+	      System.out.println("1 " +tekst1 +" "+ "1" +" "+tekst2);
+	      System.out.println(tekst3+" "+tekst4);
+		
 	}
 }
